8bcb9eecdd8c096b47d5fe925fe4870899731cd9
vatinitat <vatinitat@8db5c9aa-4b86-11de-9241-1f6e7bca54ae>
2009-05-28 13:18:38 +0000

    Gracias Lider de Soporte

---
+++ trunk/JavaParqueadero/src/javaparqueadero/Main.java
@@ -0,0 +1,29 @@
+/*
+ * Main.java
+ *
+ * Created on 28 de mayo de 2009, 08:01 AM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package javaparqueadero;
+
+/**
+ *
+ * @author Unalman
+ */
+public class Main {
+    
+    /** Creates a new instance of Main */
+    public Main() {
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
